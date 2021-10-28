435f8440e08014f0c28c7598b9f1363e005ebe65
jwyjohn <1951510@tongji.edu.cn>
2021-10-28 16:19:55 +0800

    Finished the addtion function of eval prototype.

---
+++ 07_Eval/main.cpp
@@ -0,0 +1,14 @@
+#include "eval.h"
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cout << "Testing.." << endl;
+	cin>>s;
+	exp *tes=new exp(s);
+	return 0;
+}
