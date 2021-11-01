e7097206143e7b8a7c0d2e96b5f434451a8dc942
jwyjohn <1951510@tongji.edu.cn>
2021-11-01 17:58:33 +0800

    Fixed some bug of exam.

--- 01_Exam/main.cpp
+++ 01_Exam/main.cpp
@@ -14,30 +14,35 @@ int (*fp)(op_parms parms);
 int main()
 {
 	cout << "Op N : ";
-	int op, no, ret_code;
+	int no, ret_code;
+	char op;
 	string candidate_name;
 	cin >> op;
 	int i = 0;
-	while (op != -1)
+	while (op != EOF)
 	{
 		exam_candidate *candidate_to_add = new exam_candidate;
 		switch (op)
 		{
-		case 1:
-			cin >> i >>no >> candidate_name;
+		case 'A':
+			cin >> i >> no >> candidate_name;
 			candidate_to_add->No = no;
 			candidate_to_add->name = candidate_name;
 			ret_code = insert_candidate_after(i, candidate_to_add);
 			i++;
 			break;
-		case 2:
+		case 'R':
 			cin >> no;
 			ret_code = remove_candidate(no);
 			break;
-		case 3:
+		case 'S':
 			ret_code = show_candidates();
+			break;
+		default:
+			cout << "Unknown command" << endl;
+			ret_code = 1;
 		};
-		cout << ret_code << endl;
+		cout << "Return code is" << ret_code << endl;
 		cout << "Op N : ";
 		cin >> op;
 	};
