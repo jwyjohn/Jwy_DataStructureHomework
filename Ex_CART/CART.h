d7f26572fc68d217d18263a9862994f6982341c4
jwyjohn <1951510@tongji.edu.cn>
2021-11-07 21:39:58 +0800

    Finished the split part of CART.

--- Ex_CART/CART.h
+++ Ex_CART/CART.h
@@ -56,8 +56,16 @@ struct feature_split
     }
 };
 
+struct split_pair
+{
+    feature_split split;
+    dataset A, B;
+};
+
 float calc_gini(const dataset D)
 {
+    if (D.size == 0)
+        return 1e20;
     float res = 1.0;
     vector<int> cnt;
     cnt.resize(D.category_num);
@@ -111,6 +119,8 @@ float calc_split_gini(const dataset D, int feature_no, float threshold)
         // cout << "CAT ! " << i << endl;
     };
     float res = 0.0;
+    if (A.size == 0 || B.size == 0)
+        return 1e20;
     res = float(A.size) / float(D.size) * calc_gini(A) + float(B.size) / float(D.size) * calc_gini(B);
     return res;
 };
@@ -145,8 +155,52 @@ feature_split find_split(dataset D)
         }
     };
     res = q.top();
-    cout << res.no << ": " << res.threshold << " " << res.gini << " " << endl;
+    // cout << res.no << ": " << res.threshold << " " << res.gini << " " << endl;
     return res;
 };
 
+bool cmp_split_thres(float a, float b, bool is_reverted)
+{
+    if (is_reverted)
+        return a > b;
+    else
+        return a <= b;
+};
+
+split_pair gen_split_pair(dataset D)
+{
+    split_pair res;
+    res.split = find_split(D);
+    int feature_no = res.split.no;
+    float threshold = res.split.threshold;
+    res.A.category_num = D.category_num;
+    res.A.feature_num = D.feature_num;
+    res.A.feature.resize(D.feature_num);
+    res.B.category_num = D.category_num;
+    res.B.feature_num = D.feature_num;
+    res.B.feature.resize(D.feature_num);
+    for (int i = 0; i < D.size; i++)
+    {
+        float tmp = D.feature[feature_no][i];
+        if (tmp <= threshold)
+        {
+            res.A.size++;
+            res.A.category.push_back(D.category[i]);
+            for (int j = 0; j < D.feature_num; j++)
+            {
+                res.A.feature[j].push_back(D.feature[j][i]);
+            };
+        }
+        else
+        {
+            res.B.size++;
+            res.B.category.push_back(D.category[i]);
+            for (int j = 0; j < D.feature_num; j++)
+            {
+                res.B.feature[j].push_back(D.feature[j][i]);
+            };
+        };
+    };
+    return res;
+};
 #endif
--- Ex_CART/main.cpp
+++ Ex_CART/main.cpp
@@ -12,6 +12,7 @@ int main()
 	D.feature[0] = {1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0};
 	D.feature[1] = {0.0, 1.0, 0.0, 1.0, 2.0, 1.0, 2.0, 0.0, 1.0, 0.0};
 	D.feature[2] = {125.0, 100.0, 70.0, 120.0, 95.0, 60.0, 220.0, 85.0, 75.0, 90.0};
+
 	// cout << calc_gini(D) << endl;
 	// cout << calc_split_gini(D, 2, 80.0) << endl;
 	// for (auto &item : gen_split(D, 2))
@@ -19,11 +20,41 @@ int main()
 	// 	cout << " " << item;
 	// }
 	// cout << endl;
-	// int N = 4;
-	// while (N > 0)
+
+	// find_split(D);
+
+	// split_pair tmp = gen_split_pair(D);
+	// for (auto &item : tmp.A.category)
 	// {
-	// 	N--;
+	// 	cout << " " << item;
+	// }
+	// cout << endl;
+	// for (auto &item : tmp.B.category)
+	// {
+	// 	cout << " " << item;
+	// }
+	// cout << endl;
+	int N = 4;
+	stack<split_pair> s;
+	s.push(gen_split_pair(D));
+	while (N > 0 && !s.empty())
+	{
+		N--;
+		dataset tmpA = s.top().A;
+		dataset tmpB = s.top().B;
+		cout << s.top().split.no << ": " << s.top().split.threshold << endl;
+		s.pop();
+		if (tmpA.size > 1)
+			s.push(gen_split_pair(tmpA));
+		if (tmpB.size > 1)
+			s.push(gen_split_pair(tmpB));
+		cout << " - " << tmpA.size << " " << tmpB.size << endl;
+	};
+	cout << "DONE!" << endl;
+	// while (!s.empty())
+	// {
+	// 	cout << s.top().split.no << ": " << s.top().split.threshold << endl;
+	// 	s.pop();
 	// };
-	find_split(D);
 	return 0;
 }
