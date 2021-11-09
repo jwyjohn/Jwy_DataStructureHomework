fee5f2bc109c3250fa88014e50fe56f3a927b2f6
jwyjohn <1951510@tongji.edu.cn>
2021-11-09 03:59:52 -0500

    Generated some result files. Quick sort has some errors.

--- 08_Sort_prof_data/run.sh
+++ 08_Sort_prof_data/run.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-g++ run_test.cpp -o run_test.out
+list="heap_sort bubble_sort selection_sort insertion_sort binary_insertion_sort shell_sort quick_sort bucket_sort merge_sort lsd_radix_sort msd_radix_sort"
 
 
-for i in $(ls ./test_inputs/*.in) 
+for s in $list
 do
-    echo ======================================
-    echo $i
-    ./run_test.out < $i
+    cat run_test.txt | sed "s|SED_HERE|$s|g" > tmp.cpp
+    g++ tmp.cpp -o tmp.out
+    {
+        for i in $(ls ./test_inputs/*.in) 
+        do
+            echo CASE $i
+            ./tmp.out < $i
+        done
+    } > result_$s.txt
+    echo DONE $s !
+    rm tmp.out tmp.cpp
 done
-rm run_test.out
