ac1884975500add73d3e0413ff8add1419de724c
jwyjohn <1951510@tongji.edu.cn>
2021-11-10 14:56:06 +0800

    Finished prototype of insert avl with bugs.

--- templates/avl_tree.cpp
+++ templates/avl_tree.cpp
@@ -51,29 +51,19 @@ int print_tree_vlr(avl_tree *t)
     return 0;
 };
 
-int get_balance(avl_tree *t)
-{
-    if (t->left == nullptr && t->right == nullptr)
-        return 0;
-    if (t->left == nullptr && t->right != nullptr)
-        return t->right->height;
-    if (t->left != nullptr && t->right == nullptr)
-        return -1 * t->left->height;
-    return (t->right->height - t->left->height);
-};
-
 int get_height(avl_tree *t)
 {
-    int hl = 0, hr = 0;
-    if (t->left == nullptr)
-        hl = 0;
-    else
-        hl = t->left->height + 1;
-    if (t->right == nullptr)
-        hr = 0;
-    else
-        hr = t->right->height + 1;
-    return max(hl, hr);
+    if (t == nullptr)
+        return 0;
+    return max(get_height(t->left), get_height(t->right)) + 1;
+};
+
+int get_balance(avl_tree *t)
+{
+    if (t == nullptr)
+        return 0;
+    // return (t->right->height - t->left->height);
+    return get_height(t->right) - get_height(t->left);
 };
 
 int rotate_left(avl_tree *t, avl_tree *&p)
@@ -96,13 +86,13 @@ int rotate_right(avl_tree *t, avl_tree *&p)
     return 0;
 };
 
-int adjust_tree_LL(avl_tree *t, avl_tree *&p)
+int adjust_tree_RR(avl_tree *t, avl_tree *&p)
 {
     rotate_right(t, p);
     return 0;
 };
 
-int adjust_tree_RR(avl_tree *t, avl_tree *&p)
+int adjust_tree_LL(avl_tree *t, avl_tree *&p)
 {
     rotate_left(t, p);
     return 0;
@@ -122,24 +112,111 @@ int adjust_tree_RL(avl_tree *t, avl_tree *&p)
     return 0;
 };
 
+int balance_node(avl_tree *t, avl_tree *&p)
+{
+    int bf = get_balance(t);
+    if (bf > 1)
+    {
+        if (get_balance(t->right) > 0)
+        {
+            adjust_tree_LL(t, p);
+        };
+        if (get_balance(t->right) <= 0)
+        {
+            adjust_tree_RL(t, p);
+        };
+    };
+    if (bf < -1)
+    {
+        if (get_balance(t->left) > 0)
+        {
+            adjust_tree_RR(t, p);
+        };
+        if (get_balance(t->left) <= 0)
+        {
+            adjust_tree_LR(t, p);
+        };
+    };
+    return 0;
+};
+
+avl_tree *find_val(avl_tree *t, int val)
+{
+    if (val == t->data)
+        return t;
+    if (val < t->data)
+    {
+        if (t->left == nullptr)
+            return t;
+        else
+            return find_val(t->left, val);
+    };
+    if (val > t->data)
+    {
+        if (t->right == nullptr)
+            return t;
+        else
+            return find_val(t->right, val);
+    };
+};
+
+int insert_val(avl_tree *t, int val)
+{
+    if (t->data == val)
+    {
+        cout << "Value exists!" << endl;
+        return 1;
+    }
+    else
+    {
+        if (val < t->data)
+        {
+            if (t->left == nullptr)
+            {
+                avl_tree_node *ins = new avl_tree_node;
+                ins->data = val;
+                t->left = ins;
+            }
+            else
+            {
+                insert_val(t->left, val);
+            }
+        };
+        if (val > t->data)
+        {
+            if (t->right == nullptr)
+            {
+                avl_tree_node *ins = new avl_tree_node;
+                ins->data = val;
+                t->right = ins;
+            }
+            else
+            {
+                insert_val(t->right, val);
+            }
+        };
+    };
+    balance_node(t->left, t->left);
+    balance_node(t->right, t->right);
+    return 0;
+};
+
 int main()
 {
     avl_tree T[1000];
-    for (int i = 0; i < 31; i++)
+    int i = 0;
+    int N;
+    cin >> N;
+    T[0].data = N;
+    while (true)
     {
-        T[i].data = i + 1;
-        T[i].left = &T[i * 2 + 1];
-        T[i].right = &T[i * 2 + 2];
-    };
-    print_tree_lvr(&T[0]);
-    cout << endl;
-    print_tree_vlr(&T[0]);
-    cout << endl;
+        cin >> N;
+        insert_val(&T[0], N);
+        print_tree_lvr(&T[0]);
+        cout << endl;
+        print_tree_vlr(&T[0]);
+        cout << endl;
+    }
 
-    adjust_tree_LR(T[0].left, T[0].left);
-    print_tree_lvr(&T[0]);
-    cout << endl;
-    print_tree_vlr(&T[0]);
-    cout << endl;
     return 0;
 };
