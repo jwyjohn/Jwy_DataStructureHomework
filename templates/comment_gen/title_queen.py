7d6d25f39a27ae8555dfda86446dbc1387df49b0
jwyjohn <1951510@tongji.edu.cn>
2021-11-13 05:34:51 -0500

    Finished BSortTree for both linux and windows.

--- templates/comment_gen/title_queen.py
+++ templates/comment_gen/title_queen.py
@@ -1,16 +1,16 @@
 from pyfiglet import Figlet
 
 f = Figlet(font='larry3d')
-s = f.renderText('network')
+s = f.renderText('avlTree')
 
 s+="\n - Free Software by 1951510 JiangWenyuan \nNov 2021\n"
 print(s)
-info = ["This is a program to optimize a plan for electric network.",
-        "Input command 'adv A' to add a city named A.",
-        "Input command 'ade A B 100' to add connect A WITH B at 100 million cost.",
-        "Input command 'show' to inspect the graph.",
-        "Use 'run A' to run optimization strating from city A.",
-        "Use 'init' to clear graph WITH CAUTION."]
+info = ["This is a program to operate an AVL Tree.",
+        "Input command 'add 19 7 6 4 ...' to add values to tree.",
+        "Input command 'lvr', 'lvr' or to expolre the tree.",
+        "Input command 'find 19 7 6 3...' to search value in the tree.",
+        "Use 'rm 9 7 5 3...' to remove value in tree.",
+        "Use 'init' to clear and init tree WITH CAUTION."]
 wid = len(s.split('\n')[0])
 bar = '='*wid+'\n'
 tip = "! "+'\n! '.join(info)
